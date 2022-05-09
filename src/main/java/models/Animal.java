ced15794f778ad6d932fe055ee8a5629e88aaa0a
Sylvia-Chepkoech <slycheche@gmail.com>
2022-05-09 20:50:30 +0300

    add animal class and its properties

---
+++ src/main/java/models/Animal.java
@@ -0,0 +1,10 @@
+package models;
+
+public class Animal {
+    public String id;
+    public String name;
+    public String category_type;
+
+    //creating a constant non-endangared category
+    public static final String CATEGORY_TYPE = "UNTHREATENED";
+}
