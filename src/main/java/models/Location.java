543c2198efa0bb3d6f3a7ea93d94bc83acf35912
Sylvia-Chepkoech <slycheche@gmail.com>
2022-05-10 15:49:27 +0300

    generate test location class

--- src/main/java/models/Location.java
+++ src/main/java/models/Location.java
@@ -1,4 +1,17 @@
 package models;
 
+import java.util.List;
+
 public class Location {
+    private int locationId;
+    private String locationName;
+    private String locationDescription;
+
+    private List<Sightings> sighting;
+
+
+    public Location(String locationName, String locationDescription) {
+        this.locationName = locationName;
+        this.locationDescription = locationDescription;
+    }
 }
---
+++ src/test/java/models/LocationTest.java
@@ -0,0 +1,12 @@
+package models;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
