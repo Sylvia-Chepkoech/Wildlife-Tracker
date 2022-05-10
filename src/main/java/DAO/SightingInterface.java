6c21ad38c2a4144c2a743ece3d47a8e96db1dfdd
Sylvia-Chepkoech <slycheche@gmail.com>
2022-05-10 19:25:28 +0300

    create CRUD functionality

--- src/main/java/DAO/SightingInterface.java
+++ src/main/java/DAO/SightingInterface.java
@@ -1,5 +1,22 @@
 package DAO;
 
+import models.Sightings;
+
+import java.util.List;
+
 public interface SightingInterface {
+    //CREATE
+    void add(Sightings sight);
+
+    //READ
+    Sightings getSightById(int id);
+    List<Sightings> getAllSight();
+
+    //UPDATE
+//    void update(Sightings sight);
+
+    //DELETE
+
+
 
 }
