4eeb5e5f57b2ecbe1332b622432e352bea6644b3
Sylvia-Chepkoech <slycheche@gmail.com>
2022-05-11 08:12:10 +0300

    create templates layout

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //getting hompage
+        get("/", (request, response) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        //getting sightingform
+        get("/SightingForm", (request, response) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "SightingForm.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //getting all sightings
+        get("/Sightings", (request, response) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "Sightings.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
     }
 }
